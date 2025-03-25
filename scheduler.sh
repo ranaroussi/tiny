ebcd291d008f6f35bf803d6e263ceb5c32ecbe63
Ran Aroussi <ran@aroussi.com>
2025-03-25 23:33:17 +0000

    Increase scheduler loop iterations and adjust sleep duration for improved timing

--- scheduler.sh
+++ scheduler.sh
@@ -4,12 +4,13 @@
 
 i=0
 
-while [ $i -lt 59 ]; do # 59 one-second intervals in 1 minute
+while [ $i -lt 119 ]; do # 119 one-second intervals in 1 minute
   # Get the directory of the current script
   SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
   # Use the script directory to find scheduler.php
-  php "$SCRIPT_DIR/scheduler.php" > /dev/null 2>&1 &
-  sleep 1
+#   php "$SCRIPT_DIR/scheduler.php" > /dev/null 2>&1
+  php "${SCRIPT_DIR}/scheduler.php"
+  sleep 0.5
   i=$(( i + 1 ))
 done
