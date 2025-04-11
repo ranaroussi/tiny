dcfc20464252e098d918ad612ae302e5edb688f3
Ran Aroussi <ran@aroussi.com>
2025-04-11 13:31:53 +0100

    Add error reporting and implement sendEmail method for Mailgun API

--- helpers/mailgun.php
+++ helpers/mailgun.php
@@ -1,6 +1,9 @@
 <?php
 
 declare(strict_types=1);
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
 
 use Mailgun\Mailgun;
 
@@ -34,6 +37,54 @@ class MailgunSender
             'subject' => $subject,
             'text' => $text
         ]);
+    }
+
+    /**
+     * Sends an HTML email using Mailgun API
+     *
+     * @param string $to_email Recipient email address
+     * @param string $to_name Recipient name
+     * @param string $subject Email subject
+     * @param string $html HTML content of the email
+     * @param string|null $from_email Sender email address (optional)
+     * @param string|null $from_name Sender name (optional)
+     * @param string|null $domain Mailgun domain to use (optional)
+     * @return object Response from Mailgun API
+     */
+    public function sendEmail(
+        string $to_email,
+        string $to_name,
+        string $subject,
+        string $html,
+        ?string $text = null,
+        ?string $from_email = null,
+        ?string $from_name = null,
+        ?string $domain = null,
+    ): object {
+        // Instantiate the client using the create method
+        $mailgun = Mailgun::create($this->getApiKey());
+
+        $from_email = $from_email ?? $_SERVER['MAILGUN_FROM_ADDRESS'];
+        $from_name = $from_name ?? $_SERVER['MAILGUN_FROM_NAME'];
+        $domain = $domain ?? $_SERVER['MAILGUN_DOMAIN'];
+
+        $params = [
+            'from' => $from_name .' <'. $from_email .'>',
+            'to' => $to_name .' <'. $to_email .'>',
+            'subject' => $subject,
+            'html' => $html
+        ];
+
+        if ($text !== null) {
+            $params['text'] = $text;
+        }
+
+        try {
+            return $mailgun->messages()->send($domain, $params);
+        } catch (\Exception $e) {
+            tiny::debug($e->getMessage());
+            return (object) [];
+        }
     }
 }
 
